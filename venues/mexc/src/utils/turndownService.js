55c500e9e93f51b33e30bc01b3f7b54f0ff1565b
Ross Saunders <rosssaunders@outlook.com>
2025-04-11 09:48:59 +1000

    refactor(mexc): clean up turndownService.js by removing commented-out code
    - Removed unused custom rules and comments from turndownService.js to improve code readability and maintainability.

--- venues/mexc/src/utils/turndownService.js
+++ venues/mexc/src/utils/turndownService.js
@@ -17,47 +17,4 @@ const turndownService = new TurndownService({
 
 turndownService.use([gfm, tables, strikethrough]);
 
-// turndownService.addRule('targetAnchor', {
-//   filter: 'a',
-//   replacement: (content, node) => {
-//     // index.html#order_side
-//     const href = node.getAttribute('href');
-//     const id = node.getAttribute('id');
-//     const anchor = href.split('#')[1];
-//     return `[${content}](#${anchor})`;
-//   }
-// });
-
-
-
-// turndownService.use(gfm);
-
-// // Add a custom rule to handle table cells and links
-// turndownService.addRule('tableCellWithLinks', {
-//   filter: 'td',
-//   replacement: (content, node) => {
-//     if (node instanceof dom.window.HTMLElement) {
-//       // // Get all links in the cell
-//       // const links = node.querySelectorAll('a');
-//       // let processedContent = node.innerHTML;
-      
-//       // // Process each link
-//       // links.forEach(link => {
-//       //   const href = link.getAttribute('href');
-//       //   if (href && href.startsWith('index.html#')) {
-//       //     const anchor = href.replace('index.html#', '').toLowerCase().replace(/_/g, '-');
-//       //     const linkText = link.textContent;
-//       //     processedContent = processedContent.replace(
-//       //       link.outerHTML,
-//       //       `[${linkText}](#${anchor})`
-//       //     );
-//       //   }
-//       // });
-      
-//       return `| ${processedContent} `;
-//     }
-//     return `| ${content} `;
-//   }
-// });
-
 export default turndownService;
