f2e9a7e2825f5186c98cd45b49524267f06bb974
Ross Saunders <rosssaunders@outlook.com>
2025-05-05 10:57:47 +0100

    remove unused HTX documentation and test files

--- venues/htx/docs/grab.sh
+++
@@ -1,9 +0,0 @@
-wget \
-  --mirror \
-  --convert-links \
-  --adjust-extension \
-  --page-requisites \
-  --no-parent \
-  --domains bitget.com \
-  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" \
-  https://www.htx.com/en-us/opend/newApiPages/
